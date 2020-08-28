86184c77200b62c9a8c79378bd7a03053d4a454a
unknown <lemberg.evgeny@gmail.com>
2020-08-28 19:36:03 +0300

    --

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+// middlewares
+app.set('views', './views');
+app.set("view engine", "ejs");
+
+app.use('/', express.static(__dirname + '/'));
+
+// app.use('/', express.static(path.join(__dirname, '/')));
+
+app.use(bodyParser.json());
+
+app.get('/', urlencodedParser, (req, res) => {
+    console.log('app.get "/"');
+    res.render('Package.ejs');
+    
+  });
+
+
+app.listen(PORT, () => {
+    console.log('Server has bin started PORT: ' + PORT);
+});
