f4fadd10bd15e9f77b56e3ed71742315ade2fee9
unknown <lemberg.evgeny@gmail.com>
2020-08-29 11:15:37 +0300

    --

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const urlencodedParser = bodyParser.urlencoded({
 });
 
 // middlewares
-app.set('views', './views');
+app.set('views', './public/views');
 // app.set('css', './public/css');
 // app.set('js', './public/js');
 // app.set('img', './public/img');
 app.set("view engine", "ejs");
 
-app.use('/', express.static(__dirname + '/'));
+// app.use('/', express.static(__dirname + '/'));
 // app.use(express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + '/node_modules'));
 
