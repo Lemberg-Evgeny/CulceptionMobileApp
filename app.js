d15faeb4d2261856adb0ddd3dde5b3630a5edad8
unknown <lemberg.evgeny@gmail.com>
2020-08-29 11:39:54 +0300

    --

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const urlencodedParser = bodyParser.urlencoded({
 
 // middlewares
 app.set('views', './public/views');
-// app.set('css', './public/css');
-// app.set('js', './public/js');
-// app.set('img', './public/img');
+app.set('css', './public/css');
+app.set('js', './public/js');
+app.set('img', './public/img');
 app.set("view engine", "ejs");
 
-app.use('/', express.static(__dirname + '/'));
+// app.use('/', express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
