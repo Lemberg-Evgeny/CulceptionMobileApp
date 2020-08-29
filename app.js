c6c90edbd0e8043ae1f0f6964670b16f7f0193b8
unknown <lemberg.evgeny@gmail.com>
2020-08-29 12:03:43 +0300

    ---

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const ejs=require('ejs');
 
 const app = express();
 
@@ -16,9 +17,9 @@ app.set('js', './public/js');
 app.set('img', './public/img');
 app.set("view engine", "ejs");
 
-app.use('/', express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
+// app.use('/', express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/node_modules'));
 
 // app.use("/public", express.static(__dirname + '/public'));
 
