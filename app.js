44ba723d3c16ec40f86be49822748189ad35fa31
unknown <lemberg.evgeny@gmail.com>
2020-08-29 12:36:01 +0300

    --

--- app.js
+++ app.js
@@ -19,9 +19,11 @@ app.set('img', './public/img');
 app.set("view engine", "ejs");
 
 app.use('/', express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + './views'));
+app.use(express.static('public'));
+
 
 // app.use("/public", express.static(__dirname + '/public'));
 
