7e8de87a5a7bfcfc44194e447c7cf8b18e3efcca
unknown <lemberg.evgeny@gmail.com>
2020-08-29 12:06:04 +0300

    -

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.set('js', './public/js');
 app.set('img', './public/img');
 app.set("view engine", "ejs");
 
-// app.use('/', express.static(__dirname + '/'));
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.static(__dirname + '/node_modules'));
+app.use('/', express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 
 // app.use("/public", express.static(__dirname + '/public'));
 
